6c4fcbad7aa7831fb73eb81a41027cc9f0c1a760
PC <anikin13@gmail.com>
2024-12-02 19:00:27 +0400

    1. Start

--- app/src/main/java/com/materialdesignguidelines/otus/MainActivity.kt
+++ app/src/main/java/com/materialdesignguidelines/otus/MainActivity.kt
@@ -1,14 +1,7 @@
 package com.materialdesignguidelines.otus
 
-import android.graphics.RenderEffect
-import android.graphics.Shader
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.view.View
-import android.view.ViewTreeObserver
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -19,42 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        var isHideSplashScreen = false
-
-        object : CountDownTimer(2000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {}
-            override fun onFinish() {
-                isHideSplashScreen = true
-            }
-        }.start()
-
-        val content: View = findViewById(android.R.id.content)
-        content.viewTreeObserver.addOnPreDrawListener(
-            object : ViewTreeObserver.OnPreDrawListener {
-                override fun onPreDraw(): Boolean {
-                    return if (isHideSplashScreen) {
-                        content.viewTreeObserver.removeOnPreDrawListener(this)
-                        true
-                    } else {
-                        false
-                    }
-                }
-            }
-        )
-
         setInsets()
-
-        /*if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
-            blurImage()
-        }*/
-    }
-
-    @RequiresApi(31)
-    private fun blurImage() {
-        findViewById<ImageView>(R.id.image_view).apply {
-            setRenderEffect(RenderEffect.createBlurEffect(15f, 0f, Shader.TileMode.MIRROR))
-        }
     }
 
     private fun setInsets() {
